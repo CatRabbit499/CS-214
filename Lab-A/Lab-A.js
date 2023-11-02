271153b7cffb476e2dbad4d60d6565723ed6a1f8
CatRabbit499 <Canon44444@gmail.com>
2023-11-02 12:09:30 -0400

    Lab-A Classwork

--- Lab-A/Lab-A.js
+++ Lab-A/Lab-A.js
@@ -1,3 +1,8 @@
-$(function () {
-
+$(() => {
+    $("#pic_box").children().each((index, el) => {
+        console.log(index, el, this);
+        // this.first().addEventListener('click', e => {
+        //     console.log(e);
+        // });
+    });
 });
