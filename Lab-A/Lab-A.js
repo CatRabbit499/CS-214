e7f8e1955e687968b9006879e48127219dc20cab
Nathan Meyer <Canon44444@gmail.com>
2023-11-05 13:29:02 -0500

    Lab-A Final

--- Lab-A/Lab-A.js
+++ Lab-A/Lab-A.js
@@ -1,7 +1,36 @@
 $(() => {
-    $("#pic_box").children().forEach(el => {
-        el.find("img:first").addEventListener('click', e => {
-            console.log(e);
-        });
+    let timeout = 4000;
+    let thunder = () => $(".lightning").each(function () {
+        setTimeout(() => $(this).fadeIn(250).fadeOut(250), timeout);
+        switch (timeout) {
+            case 4000:
+                timeout += 1000;
+                break;
+            case 5000:
+                timeout += 2000;
+                break;
+            case 7000:
+                timeout -= 3000;
+        }
     });
+    thunder();
+    $(".face").each(function () {
+        this.addEventListener('mousedown', e => {
+            switch (e.button) {
+                case 0:
+                    $(this).animate({
+                        backgroundPositionX: "-=367"
+                    }, 1000);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    $(this).animate({
+                        backgroundPositionX: "+=367"
+                    }, 1000);
+                    break;
+            }
+        });
+    }).on('contextmenu', e => e.preventDefault());
+    setInterval(thunder, 10000);
 });
